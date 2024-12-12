9af1c5afae864a9e6d4b76d7ce794b1d455ef2f2
Adrian-Aguilera <cruz74270@gmail.com>
2024-12-11 23:28:50 -0600

    fix: theme

--- src/views/boxMainView.vue
+++ src/views/boxMainView.vue
@@ -2,16 +2,23 @@
   <v-app-bar :elevation="0" flat class="text-roboto">
     <template v-slot:prepend>
       <v-app-bar-nav-icon >
-        <v-icon @click="changeTheme" icon="bi bi-code-slash" color="orange-darken-2"></v-icon>
+        <v-chip color="warning" @click="changeTheme" variant="outlined">
+          <v-icon icon="bi bi-code-slash"></v-icon>
+        </v-chip>
       </v-app-bar-nav-icon>
     </template>
     <v-app-bar-title>Portafolio</v-app-bar-title>
 
     <template v-slot:append>
       <div v-for="item in appbar" :key="item.contacto" class="mr-4">
-        <v-btn :href="item.to" variant="outlined">
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-btn>
+        <v-tooltip :text="item.contacto" location="bottom">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" :href="item.to" variant="outlined">
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-btn>
+          </template>
+        </v-tooltip>
+
       </div>
     </template>
   </v-app-bar>
@@ -26,7 +33,9 @@
             :key="subItem.title"
           >
             <v-list-item-title>
-              <v-icon>{{ subItem.icon }}</v-icon>
+              <v-chip :color="theme === 'dark'? 'error':'primary'">
+                <v-icon>{{ subItem.icon }}</v-icon>
+              </v-chip>
               {{ subItem.title }}
             </v-list-item-title>
           </v-list-item>
@@ -81,11 +90,17 @@ export default defineComponent({
         icon: 'mdi-linkedin',
         to: 'https://www.linkedin.com/in/adri%C3%A1n-aguilera/',
       },
-    ]
+    ],
+    colorTheme: null,
   }),
   methods: {
     changeTheme() {
       this.$store.dispatch('setTheme', this.$store.getters.theme === 'dark' ? 'light' : 'dark');
+    },
+  },
+  computed: {
+    theme() {
+      return this.$store.getters.theme;
     },
   },
 });
