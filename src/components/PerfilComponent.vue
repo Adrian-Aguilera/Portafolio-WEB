fda8340f505ff58b88f9c74be894ba176db03486
Adrian-Aguilera <cruz74270@gmail.com>
2024-12-12 23:32:00 -0600

    fix: icon perfil

--- src/components/PerfilComponent.vue
+++ src/components/PerfilComponent.vue
@@ -23,48 +23,32 @@
             <v-card-text >
                 <!--my Stacks technical skills-->
                 <span class="text-green-accent-3 d-block">~ ls -skills </span>
-                <v-card variant="text" subtitle="~ backend development" class="d-flex">
-                    <v-card-text >
-                        <v-list class="d-flex">
-                            <v-list-item v-for="(skill, index) in backendSkills" :key="index">
-                                <v-list-item-title>
-                                    <v-chip color="success" variant="tonal">
-                                        <v-icon class="mr-1">{{ skill.icon }}</v-icon>
-                                        {{ skill.title }}
-                                    </v-chip>
-                                </v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-card-text>
-                </v-card>
-                <v-card variant="text" subtitle="~ frontend development" class="d-flex">
-                    <v-card-text >
-                        <v-list class="d-flex">
-                            <v-list-item v-for="(skill, index) in frontendSkills" :key="index">
-                                <v-list-item-title>
-                                    <v-chip color="success" variant="tonal">
-                                        <v-icon class="mr-1">{{ skill.icon }}</v-icon>
-                                        {{ skill.title }}
-                                    </v-chip>
-                                </v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-card-text>
-                </v-card>
-                <v-card variant="text" subtitle="~ scripting development" class="d-flex">
-                    <v-card-text >  
-                        <v-list class="d-flex">
-                            <v-list-item v-for="(skill, index) in scriptingSkills" :key="index">
-                                <v-list-item-title>
-                                    <v-chip color="success" variant="tonal">
-                                        <v-icon class="mr-1">{{ skill.icon }}</v-icon>
-                                        {{ skill.title }}
-                                    </v-chip>
-                                </v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-card-text>
-                </v-card>
+                <div>
+                    <v-card v-for="(skill, index) in skills" :key="index" variant="text" :subtitle="`~ ${skill.title} development`" class="d-flex">
+                        <v-card-text >
+                            <v-list class="d-flex">
+                                <v-list-item v-for="(technology, index) in skill.technologies" :key="index">
+                                    <v-list-item-title>
+                                        <v-chip color="success" variant="tonal">
+                                            <v-icon class="mr-1">{{ technology.icon }}</v-icon>
+                                            {{ technology.title }}
+                                        </v-chip>
+                                    </v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                            <v-list class="d-flex">
+                                <v-list-item v-for="(framework, index) in skill.frameworks" :key="index">
+                                    <v-list-item-title>
+                                        <v-chip color="success" variant="tonal">
+                                            <v-icon class="mr-1">{{ framework.icon }}</v-icon>
+                                            {{ framework.title }}
+                                        </v-chip>
+                                    </v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-card-text>
+                    </v-card>
+                </div>
             </v-card-text>
 
           </v-card-text>
@@ -73,6 +57,7 @@
 </template>
 
 <script>
+import 'github-calendar/dist/github-calendar-responsive.css';
 export default {
     name: 'PerfilComponent',
     props: {
@@ -86,40 +71,65 @@ export default {
         },
     },
     data: () => ({
-        backendSkills: [
+        skills: [
             {
-                title: 'Django',
-                icon: 'bi bi-app',
-            },
-            {
-                title: 'Django REST Framework',
-                icon: 'bi bi-app',
-            },
-        ],
-        frontendSkills: [
-            {
-                title: 'Vue.js',
-                icon: 'bi bi-app',
-            },
-            {
-                title: 'Vuetify',
-                icon: 'bi bi-app',
-            },
-        ],
-        scriptingSkills: [
-            {
-                title: 'Python',
-                icon: 'bi bi-app',
-            },
-            {
-                title: 'Bash',
-                icon: 'bi bi-app',
+                title: 'Stacks',
+                technologies: [
+                    {
+                        title: 'Python',
+                        icon: 'mdi mdi-language-python',
+                    },
+                    {
+                        title: 'Javascript',
+                        icon: 'mdi mdi-language-javascript',
+                    },
+                    {
+                        title: 'php',
+                        icon: 'mdi mdi-language-php',
+                    },
+                    {
+                        title: 'Wordpress',
+                        icon: 'bi bi-wordpress',
+                    },
+                    {
+                        title: 'Ubuntu',
+                        icon: 'bi bi-ubuntu',
+                    },
+                    {
+                        title: 'Digital Ocean',
+                        icon: 'bi bi-cloud-plus-fill',
+                    },
+                ],
+                frameworks: [
+                    {
+                        title: 'Django',
+                        icon: 'bi bi-filetype-py',
+                    },
+                    {
+                        title: 'Vue.js',
+                        icon: 'mdi mdi-vuejs',
+                    },
+                    {
+                        title: 'vuetify',
+                        icon: 'mdi mdi-vuetify',
+                    },
+                    {
+                        title: 'Angular',
+                        icon: 'mdi mdi-angular',
+                    },
+                    {
+                        title: 'ionic',
+                        icon: 'bi bi-terminal',
+                    },
+                    {
+                        title: 'Git-Github',
+                        icon: 'mdi mdi-github',
+                    },
+                ],
             },
         ],
     }),
-    methods: {
-        
-    },
+    methods: {},
     computed: {
         theme() {
             return this.$store.getters.theme;
