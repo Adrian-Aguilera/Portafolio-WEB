f0d0edccacc611d09032737965e7aeefdb2a81dc
Adrian-Aguilera <cruz74270@gmail.com>
2024-12-13 20:47:51 -0600

    feat: data

--- src/views/projectsView.vue
+++ src/views/projectsView.vue
@@ -15,7 +15,7 @@ export default {
                 name: 'Portafolio Web',
                 description: "This is a project that I'm currently working on. It's a web application that allows see the portfolio of my work.",
                 github: 'https://github.com/Adrian-Aguilera',
-                urlImage: 'https://github-readme-stats.vercel.app/api?username=Adrian-Aguilera&show_icons=true&theme=dracula&locale=en&hide_border=false',
+                urlImage:require('@/assets/img/image.png'),
                 vsCode: 'https://github.dev/Adrian-Aguilera/Portafolio-WEB',
                 technologies: [
                     {
@@ -24,7 +24,7 @@ export default {
                         href: 'https://www.w3schools.com/js/default.asp',
                     },
                     {
-                        title: 'Python',
+                        title: 'Vue.js',
                         disabled: false,
                         href: 'https://www.w3schools.com/js/default.asp',
                     },
@@ -41,7 +41,44 @@ export default {
                         icon: 'bi bi-terminal-fill',
                     },
                 ],
-                date: '2 DAY AGO UPDATED',
+                date: '1 DAY AGO UPDATED',
+            },
+            {
+                name: 'Edula-Web',
+                description: "This is a project Develop the 2024 innovation project as a lead programmer, adapting artificial intelligence technology for the educational section",
+                github: 'https://github.com/Adrian-Aguilera/EdulA-Engine',
+                urlImage:require('@/assets/img/edula.png'),
+                vsCode: 'https://github.dev/Adrian-Aguilera/EdulA-Engine',
+                technologies: [
+                    {
+                        title: 'python',
+                        disabled: false,
+                        href: 'https://www.w3schools.com/py/default.asp',
+                    },
+                    {
+                        title: 'Django',
+                        disabled: false,
+                        href: 'https://www.w3schools.com/py/default.asp',
+                    },
+                    {
+                        title: 'Django REST Framework',
+                        disabled: false,
+                        href: 'https://www.w3schools.com/py/default.asp',
+                    },
+                ],
+                status: [
+                    {
+                        title: 'Done',
+                        color: 'success',
+                        icon: 'bi bi-cpu-fill',
+                    },
+                    {
+                        title: 'DEVELOPMENT',
+                        color: 'warning',
+                        icon: 'bi bi-terminal-fill',
+                    },
+                ],
+                date: '5 DAYs AGO UPDATED',
             },
         ],
         content: "A list of projects I've worked on in the last years. I'm currently working on a new project, so I'll add it here soon.",
