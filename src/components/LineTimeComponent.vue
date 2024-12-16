e89c0ad58fbf8bea77173ea568a3960d8edb0f7b
Adrian-Aguilera <cruz74270@gmail.com>
2024-12-15 21:25:27 -0600

    fix: nuevo diseño

--- src/components/LineTimeComponent.vue
+++ src/components/LineTimeComponent.vue
@@ -8,42 +8,39 @@
                 {{ content }}
             </p>
             <v-divider :thickness="5" class="pb-4"></v-divider>
-            <v-virtual-scroll
-                :items="items"
-                item-height="100"
-                height="55vh"
-            >
-                <template v-slot="{ item, index }">
+            <v-data-iterator :items="items" :items-per-page="itemsPerPage">
+                <template v-slot:default="{ items }">
                     <v-timeline side="end">
                         <v-timeline-item
+                        v-for="(item, index) in items"
                         :key="index"
                         dot-color="green-accent-3"
                         size="small"
                       >
                         <v-card class="pa-2" :variant="isDark ? 'outlined' : 'outlined'" width="90%">
                           <v-card-title>
-                            <span>$ </span>{{ `${item.cargo}` }}
+                            <span>$ </span>{{ `${item.raw.cargo}` }}
                           </v-card-title>
                           <v-card-subtitle class="d-flex justify-space-between">
                             <div>
                                 <v-icon class="mr-2" color="green-accent-3">bi bi-buildings-fill</v-icon>
-                                <span class="justify-center">{{ `${item.empresa}` }} </span>
+                                <span class="justify-center">{{ `${item.raw.empresa}` }} </span>
                             </div>
                             <div>
                                 <v-chip :color="isDark ? 'green-accent-3' : 'error'" variant="tonal">
                                     <v-icon class="mr-1" >mdi-calendar-clock</v-icon>
-                                    {{ `${item.fecha[0].inicio}` }} ~ {{ `${item.fecha[0].fin}` }}
+                                    {{ `${item.raw.fecha[0].inicio}` }} ~ {{ `${item.raw.fecha[0].fin}` }}
                                 </v-chip>
                             </div>
                           </v-card-subtitle>
                           <v-card-text>
                             <p class="pb-5">
-                                {{ `${item.descripcion}` }}
+                                {{ `${item.raw.descripcion}` }}
                             </p>
                             <v-divider :thickness="5" class="pb-4"></v-divider>
                             <span class="text-green-accent-3">~ ls </span><span class="text-grey-lighten-1"> Featured tasks</span>
                             <v-list class="d-flex" >
-                                <v-list-item v-for="(tarea, index) in item.tareas" :key="index">
+                                <v-list-item v-for="(tarea, index) in item.raw.tareas" :key="index">
                                     <v-list-item-title>
                                         • {{ tarea.tarea }}
                                     </v-list-item-title>
@@ -54,7 +51,28 @@
                       </v-timeline-item>
                     </v-timeline>
                 </template>
-            </v-virtual-scroll>
+                <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
+                    <div class="d-flex align-center justify-center pa-4">
+                      <v-btn
+                        :disabled="page === 1"
+                        density="comfortable"
+                        icon="bi bi-caret-left-fill"
+                        variant="tonal"
+                        rounded
+                        class="mr-2"
+                        @click="prevPage"
+                      ></v-btn>
+                      <v-btn
+                        :disabled="page >= pageCount"
+                        density="comfortable"
+                        icon="bi bi-caret-right-fill"
+                        variant="tonal"
+                        rounded
+                        @click="nextPage"
+                      ></v-btn>
+                    </div>
+                  </template>
+            </v-data-iterator>
             <v-divider :thickness="5" class="pb-4"></v-divider>
         </v-card-text>
         <span class="d-flex justify-end mr-3 text-orange-darken-2 text-h5 mb-1"><v-icon>bi bi-alt</v-icon></span>
@@ -72,54 +90,13 @@ export default {
             type: String,
             default: 'This is a box component',
         },
+        items: {
+            type: Array,
+            default: () => [],
+        },
     },
     data: () => ({
-        items: [
-            {
-                cargo: 'Frontend Developer',
-                empresa: 'El salvador',
-                descripcion: "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.",
-                fecha: [
-                    {
-                        inicio: '2022-01-01',
-                        fin: '2022-01-31',
-                    },
-                ],
-                tareas: [
-                    {
-                        tarea: 'Desarrollo Frontend',
-                    },
-                    {
-                        tarea: 'Desarrollo Backend',
-                    },
-                    {
-                        tarea: 'Desarrollo Mobile',
-                    },
-                ],
-            },
-            {
-                cargo: 'Frontend Developer',
-                empresa: 'El salvador',
-                descripcion: "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.",
-                fecha: [
-                    {
-                        inicio: '2022-01-01',
-                        fin: '2022-01-31',
-                    },
-                ],
-                tareas: [
-                    {
-                        tarea: 'Desarrollo Frontend',
-                    },
-                    {
-                        tarea: 'Desarrollo Backend',
-                    },
-                    {
-                        tarea: 'Desarrollo Mobile',
-                    },
-                ],
-            },
-        ],
+        itemsPerPage: 2,
     }),
     methods: {
         
--- src/views/worksView.vue
+++ src/views/worksView.vue
@@ -1,5 +1,5 @@
 <template>
-    <LineTimeComponent  title="Works" :content="content"></LineTimeComponent>
+    <LineTimeComponent  title="Works" :content="content" :items="items" />
 </template>
 
 <script>
@@ -11,6 +11,52 @@ export default {
     },
     data: () => ({
         content: "A list of works I've done in the last years. I'm currently working on a new project, so I'll add it here soon.",
+        items: [
+            {
+                cargo: 'Frontend Developer',
+                empresa: 'El salvador',
+                descripcion: "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.",
+                fecha: [
+                    {
+                        inicio: '2022-01-01',
+                        fin: '2022-01-31',
+                    },
+                ],
+                tareas: [
+                    {
+                        tarea: 'Desarrollo Frontend',
+                    },
+                    {
+                        tarea: 'Desarrollo Backend',
+                    },
+                    {
+                        tarea: 'Desarrollo Mobile',
+                    },
+                ],
+            },
+            {
+                cargo: 'Frontend Developer',
+                empresa: 'El salvador',
+                descripcion: "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.",
+                fecha: [
+                    {
+                        inicio: '2022-01-01',
+                        fin: '2022-01-31',
+                    },
+                ],
+                tareas: [
+                    {
+                        tarea: 'Desarrollo Frontend',
+                    },
+                    {
+                        tarea: 'Desarrollo Backend',
+                    },
+                    {
+                        tarea: 'Desarrollo Mobile',
+                    },
+                ],
+            },
+        ],
     }),
     methods: {
         
