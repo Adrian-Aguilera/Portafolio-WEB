a54c783ef13f6d12e82188a11b4bf79e9c9b6a7c
Adrian-Aguilera <cruz74270@gmail.com>
2025-03-24 22:58:20 -0600

    feat: agregar nuevas experiencias laborales en la vista de trabajos

--- src/views/worksView.vue
+++ src/views/worksView.vue
@@ -31,6 +31,42 @@ export default {
                     },
                 ],
             },
+            {
+                cargo: 'Frontend Developer',
+                empresa: 'El salvador',
+                descripcion: "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.",
+                inicio: '2022-01-01',
+                fin: '2022-01-31',
+                tareas: [
+                    {
+                        tarea: 'a list of works I\'ve done in the last years. I\'m currently working on a new project, so I\'ll add it here soon. Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.',
+                    },
+                    {
+                        tarea: 'Desarrollo Backend',
+                    },
+                    {
+                        tarea: 'Desarrollo Mobile',
+                    },
+                ],
+            },
+            {
+                cargo: 'Frontend Developer',
+                empresa: 'El salvador',
+                descripcion: "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.",
+                inicio: '2022-01-01',
+                fin: '2022-01-31',
+                tareas: [
+                    {
+                        tarea: 'a list of works I\'ve done in the last years. I\'m currently working on a new project, so I\'ll add it here soon. Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.',
+                    },
+                    {
+                        tarea: 'Desarrollo Backend',
+                    },
+                    {
+                        tarea: 'Desarrollo Mobile',
+                    },
+                ],
+            },
         ],
         items: [],
     }),
